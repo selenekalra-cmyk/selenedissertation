dd8b1ed7750907080e99e40d4e58199269db678c
selenekalra-cmyk <selenekalra@gmail.com>
2026-04-02 14:29:50 +0100

    initial commit

--- GitTest.R
+++ GitTest.R
@@ -1,1 +1,9 @@
+setwd("/Users/selenekalra/Desktop/UofE/Dissertation/selenedissertation")
 read.csv("4_group.csv")
+read.csv("Master_Kelp_data.csv")
+
+
+$ git clone https://github.com/selenekalra-cmyk/selenedissertation
+Cloning into 'repo'...
+Username for 'https://github.com': selenekalra-cmyk
+Password for 'https://username@github.com': your_access_token
